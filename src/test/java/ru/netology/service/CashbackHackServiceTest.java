54334a4762e8ee481a3436f03777358781f6700b
dmitry <whtperson@gmail.com>
2023-07-18 21:49:46 +0300

    commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,2 +1,22 @@
-package ru.netology.service;public class CashbackHackServiceTest {
+package ru.netology.service;
+
+
+public class CashbackHackServiceTest {
+    @Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainWhitCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        Assert.assertEquals(actual, expected);
+    }
 }
